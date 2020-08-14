0e41c36047237600e5fb09324baaf119681c3f34
milkcat1994 <milkcat1994@gmail.com>
2020-08-14 13:36:01 +0900

    Edit Back-End: Redis Background Run
    Redis에 디비 자료를 올릴때 1시간을 주기로 정보 업데이트
    Edit S03P13A509-79

---
+++ backend/src/main/java/com/web/editor/batch/AsyncTaskService.java
@@ -0,0 +1,45 @@
+package com.web.editor.batch;
+
+import com.web.editor.model.service.request.RequestService;
+import com.web.editor.model.service.user.search.SearchService;
+import com.web.editor.model.service.user.search.redis.SearchRedisService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AsyncTaskService {
+    
+	@Autowired
+	SearchRedisService searchRedisService;
+
+	@Autowired
+	SearchService searchService;
+
+	@Autowired
+    RequestService requestService;
+    
+    /*
+     * 해당 파일에서 백그라운드에서 실행할 로직을 구현한다.
+     */
+	// 초 * 1000
+	// 1시간마다 갱신으로 설정
+	@Scheduled(fixedDelay = 360000)
+	public void SaveDatatoRedis() {
+        System.out.println("Thread Start");
+        
+		// long res = searchRedisService.deleteAll();
+		// System.out.println("지워진수>>>>"+res);
+		
+		// DB내용 Redis로 끌어올 필요 있음. 
+		searchRedisService.portfolioAndBookmarkSave(searchService.joinBookmarks());
+		searchRedisService.portfolioAndVideoSave(searchService.joinVideos());
+		searchRedisService.requestAndReviewSave(requestService.joinScores());
+		searchRedisService.makeUserInfo();
+		searchRedisService.searchRequestVideoInfoSave(requestService.searchRequestVideoInfo());
+        searchRedisService.portfolioTagSave(searchService.searchPortfolioTag());
+        
+		System.out.println("Thread End");
+	}
+}
